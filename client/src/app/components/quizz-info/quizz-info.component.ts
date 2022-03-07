610bdb13f085ed96689ec8c3bc8255ade6c60f4a
Diego EM <dmunozaguilar2000@gmail.com>
2022-03-06 23:37:52 -0700

    Main menu added

---
+++ client/src/app/components/quizz-info/quizz-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz-info',
+  templateUrl: './quizz-info.component.html',
+  styleUrls: ['./quizz-info.component.css']
+})
+export class QuizzInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
